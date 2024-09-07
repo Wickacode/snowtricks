556a7c45772cd1413f39519ff48662313ac532f5
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-09-07 02:02:22 +0200

    Preparation of all necessary templates and controllers. Add style to navbar and home page

---
+++ src/Controller/TricksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TricksController extends AbstractController
+{
+    #[Route('/trick', name: 'app_trick')]
+    public function showTrick(): Response
+    {
+        return $this->render('tricks/trick.html.twig', [
+            'controller_name' => 'TricksController',
+        ]);
+    }
+}
